98fd5fc03694c0f25eae529c287bef31457f6958
Simon Beguin <simon.beguin@student.isae-supaero.fr>
2022-04-19 09:24:19 +0200

    RE : Ajout du template login simpliste
    Le templates n'était pas au bon endroit donc je l'ai déplacer et j'ai ajouté la route correspondante

--- topoguide/topoguide/settings.py
+++ topoguide/topoguide/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'itineraires.apps.ItinerairesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- topoguide/topoguide/urls.py
+++ topoguide/topoguide/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/login/', LoginView.as_view(template_name='registration/login.html')),
 ]
