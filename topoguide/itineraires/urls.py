837ca5ac42339cb48295d613c64fb9375f5c8f5b
Simon Beguin <simon.beguin@student.isae-supaero.fr>
2022-04-21 09:41:13 +0200

    Ajout commentaires et modif nom path

--- topoguide/itineraires/urls.py
+++ topoguide/itineraires/urls.py
@@ -6,11 +6,16 @@ from .import views
 
 app_name = 'itineraires'
 urlpatterns = [
+    # ex: /itineraires/
     path('', views.itineraires, name='itineraires'),
-    path('sorties/<int:itineraire_id>/', views.sorties, name='sorties'),
+    # ex: /itineraires/1/
+    path('sorties/<int:itineraire_id>/', views.sorties, name='sorties')
+    # ex: /itineraires/sortie/3/
     path('sortie/<int:sortie_id>/', views.sortie, name='sortie_details'),
-    path('sortie/nouvelle_sortie/', views.nouvelle_sortie, name='nouvelle_sortie'),
-    path('sortie/<int:sortie_id>/editer/', views.modif_sortie, name='modif_sortie'),
+    # ex: /itineraires/nouvelle_sortie/
+    path('nouvelle_sortie/', views.nouvelle_sortie, name='nouvelle_sortie'),
+    # ex: /itineraires/modif_sortie/2/
+    path('modif_sortie/<int:sortie_id>/', views.modif_sortie, name='modif_sortie'),
 
 
     ] 
